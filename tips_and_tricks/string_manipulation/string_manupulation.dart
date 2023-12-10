fab3c7ae8f0928863939e0d900b9507d32ffe0d9
Nehal Babu <inehalbabu@gmail.com>
2023-12-10 18:45:55 +0530

    added string manipulation tips and tricks

---
+++ tips_and_tricks/string_manipulation/string_manupulation.dart
@@ -0,0 +1,14 @@
+/// Did you know dart support string manipulation?
+///
+void main() {
+  for (var i = 1; i <= 5; i++) {
+    print('🎄' * i);
+  }
+}
+
+/// results:
+// 🎄
+// 🎄🎄
+// 🎄🎄🎄
+// 🎄🎄🎄🎄
+// 🎄🎄🎄🎄🎄
