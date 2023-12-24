9bb111a08c823d451cefc0beffb28a9d4dec400c
Nehal Babu <102398951+inehalbabu@users.noreply.github.com>
2023-12-24 19:22:26 +0530

    use final and const in dart

---
+++ tips_and_tricks/final_const/final_const.dart
@@ -0,0 +1,6 @@
+/// Use the final and const Keywords:
+/// - Use final for variables that won't change their value after initialization.
+/// - Use const for constant values.
+///
+final int x = 10;
+const double pi = 3.14;
