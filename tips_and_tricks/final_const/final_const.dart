c76703f3cb5ed31ad1035603df8353cc330a52b6
Nehal Babu <102398951+inehalbabu@users.noreply.github.com>
2024-07-14 17:12:18 +0530

    added a example for final and const

--- tips_and_tricks/final_const/final_const.dart
+++ tips_and_tricks/final_const/final_const.dart
@@ -1,6 +1,10 @@
-/// Use the final and const Keywords:
-/// - Use final for variables that won't change their value after initialization.
-/// - Use const for constant values.
-///
-final int x = 10;
-const double pi = 3.14;
+void main() {
+  final int x = 10; // Declaring 'x' as final means its value can't change after initialization.
+  const double pi = 3.14; // Declaring 'pi' as const ensures it's a compile-time constant.
+
+  // Trying to reassign final variables will result in a compile-time error.
+  // x = 20; // Error: The final variable 'x' can only be set once.
+
+  print('x: $x');
+  print('pi: $pi');
+}
